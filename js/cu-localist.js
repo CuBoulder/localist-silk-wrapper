178e24beaed3d27ee920b1d0661dbd874382daac
kevincrafts <kevin.crafts@colorado.edu>
2017-10-27 15:18:47 -0600

    adding js to move top featured event to be full width, other small css fixes

---
+++ js/cu-localist.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var spotlight = $('.event-spotlight-component').length;
+  var slider = $('.featured_section_component ').length;
+  slider = 0;
+  spotlight = 0;
+  if (spotlight > 0) {
+    $('.event-spotlight-component:first').appendTo('.featured-container');
+  }
+  else if (slider > 0) {
+    $('.featured_section_component:first').appendTo('.featured-container');
+    $('.featured_item a.box_left img').each(function(){
+      var url = $(this).attr('src');
+      url = url.replace('/big/', '/cover/');
+      $(this).attr('src', url);
+    });
+  }
+});
